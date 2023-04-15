103058dcf5eca4342b75804bd0478c759ac2f45c
kth-tw <88102592+kth-tw@users.noreply.github.com>
2023-04-15 14:48:00 +0900

    add couchdb

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
 import { MerkleTree } from 'merkletreejs'
 // @ts-ignore
 import { buildPoseidon } from 'circomlibjs'
+import nano from 'nano'
+
+const couchdb = nano('http://admin:password@localhost:5984').use('merkle-tree')
+const insertIfNotExist = async (root: string, layers: string[][], leaves: any[]) => {
+  try {
+    await couchdb.get(root)
+  } catch (err) {
+    if (err) {
+      // @ts-ignore
+      if (err?.statusCode === 404) {
+        await couchdb.insert({
+          // @ts-ignore
+          layers,
+          leaves,
+        }, root)
+      } else {
+        throw err
+      }
+    }
+  }
+}
 
 const getPoseidonHashFunction = async () => {
   const poseidon = await buildPoseidon()
@@ -25,6 +46,12 @@ const anyToBigNumber = (input: any) => {
 }
 
 const getInitSMT = async () => {
+  try {
+    // @ts-ignore
+    const { leaves, layers } = await couchdb.get('0x2f194faeacf4af40d85a0990eb479cae2bc7f60e8be98153430bc352bbd12da3')
+    return { leaves, layers }
+  } catch (e) {}
+
   const hashFunction = await getPoseidonHashFunction()
   const leaves = [
     -1,
@@ -38,13 +65,15 @@ const getInitSMT = async () => {
   ]
   const tree = new MerkleTree(leaves, hashFunction, { hashLeaves: true, concatenator: (hashes) => hashes })
   const layers = (tree.getLayers() as unknown as Buffer[][]).map(layer => layer.map(node => MerkleTree.bufferToHex(node)))
+  await insertIfNotExist(layers[layers.length - 1][0], layers, leaves)
   return {
     layers,
     leaves,
   }
 }
 
-const updateSMT = async (leaves: any[], layers: string[][], data: {index: number; value: any}[]) => {
+const createSMT = async (data: {index: number; value: any}[]) => {
+  const { leaves, layers } = await getInitSMT()
   const hashFunction = await getPoseidonHashFunction()
 
   const newLayers: string[][] = []
@@ -68,13 +97,14 @@ const updateSMT = async (leaves: any[], layers: string[][], data: {index: number
       }
     }
   }
+  await insertIfNotExist(newLayers[newLayers.length - 1][0], newLayers, leaves)
   return {
     layers: newLayers,
     leaves: leaves,
   }
 }
 
-const proofByIndex = (index: number, value: any, layers: string[][]) => {
+const proofByIndex = async (index: number, value: any, root: string) => {
   const key = ([
     'certUrl',
     'certId',
@@ -85,6 +115,9 @@ const proofByIndex = (index: number, value: any, layers: string[][]) => {
     'slotA',
     'slotB',
   ])[index]
+
+  // @ts-ignore
+  const { layers } = await couchdb.get(root)
 
   const path: string[] = []
   const idx: number[] = []
